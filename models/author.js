dda65f4368f62324670052323a72e1b40bc9c467
Roshan <roshansharma406@gmail.com>
2020-03-30 17:15:44 +0530

    Fished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
+const Book = require('./book');
 
 const authorsSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
   }
+});
+
+authorsSchema.pre('remove', function(next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err);
+    } else if (books.length > 0) {
+      next(new Error('This Author has book Still'));
+    } else {
+      next();
+    }
+  });
 });
 
 module.exports = mongoose.model('Author', authorsSchema);
