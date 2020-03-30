38a610acf652421773ca31d48ec1a76205d4de68
Roshan <roshansharma406@gmail.com>
2020-03-30 15:25:53 +0530

    file Upload Database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,13 @@
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginImageResize,
+  FilePondPluginFileEncode
+);
+
+FilePond.setOptions({
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetWidth: 100,
+  imageResizeTargetHight: 150
+});
+
+FilePond.parse(document.body);
