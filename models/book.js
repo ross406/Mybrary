28d97b045bf764e665049615bc087fb8e038d196
Roshan <roshansharma406@gmail.com>
2020-04-05 12:24:59 +0530

    Description Bug Fixed

--- models/book.js
+++ models/book.js
@@ -3,40 +3,40 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
-  discription: {
-    type: String
+  description: {
+    type: String,
   },
   publishDate: {
     type: Date,
-    required: true
+    required: true,
   },
   pageCount: {
     type: Number,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
     required: true,
-    default: Date.now
+    default: Date.now,
   },
   coverImage: {
     type: Buffer,
-    required: true
+    required: true,
   },
   coverImageType: {
     type: String,
-    required: true
+    required: true,
   },
   author: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'Author'
-  }
+    ref: 'Author',
+  },
 });
 
-bookSchema.virtual('coverImagePath').get(function() {
+bookSchema.virtual('coverImagePath').get(function () {
   if (this.coverImage != null && this.coverImageType != null) {
     return `data:${this.coverImageType};charset=utf-8;base64,
     ${this.coverImage.toString('base64')}`;
